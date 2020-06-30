86d2af6a1b65d7944447e73ac0686127b8fcd635
zxb <18701012039@163.com>
2020-06-30 16:12:36 +0800

    Revert "统一返回和统一异常处理"
    This reverts commit f6b361fc

---
+++ service/src/main/java/com/zxb/category/Gitteat.java
@@ -0,0 +1,15 @@
+package com.zxb.category;/**
+ * @author zxb
+ * @date 2018/10/19
+ */
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author admin
+ * @create 2020/6/30
+ * @since 1.0.0
+ */
+public class Gitteat {
+}
--- service/src/main/java/com/zxb/domain/GlobalDefaultProperties.java
+++ service/src/main/java/com/zxb/domain/GlobalDefaultProperties.java
@@ -1,22 +1,38 @@
 package com.zxb.domain;
 
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.ArrayList;
 import java.util.List;
 
 @ConfigurationProperties(GlobalDefaultProperties.PREFIX)
-@Data
 public class GlobalDefaultProperties {
 
     public static final String PREFIX = "dispose";
 
-    @ApiModelProperty(value ="统一返回过滤包" )
+    /**
+     * 统一返回过滤包
+     */
     private List<String> adviceFilterPackage = new ArrayList<>();
 
-    @ApiModelProperty(value = "统一返回过滤类")
+    /**
+     * 统一返回过滤类
+     */
     private List<String> adviceFilterClass = new ArrayList<>();
 
+    public List<String> getAdviceFilterPackage() {
+        return adviceFilterPackage;
+    }
+
+    public void setAdviceFilterPackage(List<String> adviceFilterPackage) {
+        this.adviceFilterPackage = adviceFilterPackage;
+    }
+
+    public List<String> getAdviceFilterClass() {
+        return adviceFilterClass;
+    }
+
+    public void setAdviceFilterClass(List<String> adviceFilterClass) {
+        this.adviceFilterClass = adviceFilterClass;
+    }
 
 }
