6f78998fc732a9a5b1de31d9aa4c6b3a4de96e14
zxb <18701012039@163.com>
2020-06-30 16:14:53 +0800

    bug修复

--- service/src/main/java/com/zxb/domain/GlobalDefaultProperties.java
+++ service/src/main/java/com/zxb/domain/GlobalDefaultProperties.java
@@ -1,21 +1,22 @@
 package com.zxb.domain;
 
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.ArrayList;
 import java.util.List;
 
 @ConfigurationProperties(GlobalDefaultProperties.PREFIX)
-@Data
 public class GlobalDefaultProperties {
 
     public static final String PREFIX = "dispose";
 
-    @ApiModelProperty(value ="统一返回过滤包" )
+    /**
+     * 统一返回过滤包
+     */
     private List<String> adviceFilterPackage = new ArrayList<>();
 
-    @ApiModelProperty(value = "统一返回过滤类")
+    /**
+     * 统一返回过滤类
+     */
     private List<String> adviceFilterClass = new ArrayList<>();
 
 
